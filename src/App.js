c36ad04c0fef13bcda1e16f6de8b3753dec0b196
mcittanti <mcittanti@udesa.edu.ar>
2026-04-01 17:21:38 -0300

    Rutas Peliculas y series, header/ footer series

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Home from "./Screens/Home/Home";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 import Peliculas from './Screens/Peliculas/Peliculas';
+import Series from './Screens/Series/Series';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Switch>
           <Route path="/" exact = {true} component={Home}/>
           <Route path="/Peliculas" exact = {true} component={Peliculas}/>
+          <Route path="/Series" exact = {true} component={Series}/>
         </Switch>
       </div>
     </BrowserRouter>
---
+++ src/Screens/Series/Series.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "../../Components/Footer/Footer"
+import Header from "../../Components/Header/Header"
+
+function Series () {
+    return (
+        <div>
+            <Header />
+            <Footer />
+        </div>
+    )
+}
+
+export default Series;
