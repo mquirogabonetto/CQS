353984f0cf548b42c84ec9fb888b18a1576ad3d4
mcittanti <mcittanti@udesa.edu.ar>
2026-04-01 17:25:08 -0300

    Rutas

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Home from "./Screens/Home/Home";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 import Peliculas from './Screens/Peliculas/Peliculas';
 import Series from './Screens/Series/Series';
+import Favoritos from './Screens/Favoritos/Favoritos';
+import LogIn from './Screens/LogIn/LogIn';
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
           <Route path="/" exact = {true} component={Home}/>
           <Route path="/Peliculas" exact = {true} component={Peliculas}/>
           <Route path="/Series" exact = {true} component={Series}/>
+          <Route path="/Favoritos" exact = {true} component={Favoritos}/>
+          <Route path="/LogIn" exact = {true} component={LogIn}/>
         </Switch>
       </div>
     </BrowserRouter>
---
+++ src/Screens/Favoritos/Favoritos.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "../../Components/Footer/Footer"
+import Header from "../../Components/Header/Header"
+
+function Favoritos () {
+    return (
+        <div>
+            <Header />
+            <Footer />
+        </div>
+    )
+}
+
+export default Favoritos;
