2011829669ca3aaf35f6584b04a81695a8f28b59
mquirogabonetto <mquirogabonetto@udesa.edu.ar>
2026-04-13 20:44:11 -0300

    Código y css de parte SignUp

---
+++ src/Components/SignUpForm/SignUpForm.js
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+
+class SignUpForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "",
+            email: "",
+            password: "",
+            errorUsername: "",
+            errorEmail: "",
+            errorPassword: ""
+        };
+    }
+
+    submit(event) {
+        event.preventDefault ();
+         
+        let usuarioACrear = {
+            username: this.state.username,
+            email: this.state.email,
+            password: this.state.password,
+            createdAt: Date.now(),
+        };
+        if (usuarioACrear.username.length < 3 || usuarioACrear.username.length > 7) {
+            this.setState({errorUsername: "La extensión del username debe ser de 3 a 7 caracteres"});
+            return;
+        } else {
+            this.setState({
+                errorUsername: ""
+            })
+        }
+        if (!usuarioACrear.email.includes("@")){
+            this.setState({errorEmail: "Email mal formateado"});
+            return;
+        } else {
+            this.setState({
+                errorEmail: ""
+            })
+        }
+        if (usuarioACrear.password.length < 5 ||usuarioACrear.password.length > 12){
+            this.setState({errorPassword: "La extensión del password debe ser de 5 a 12 caracteres"});
+            return;
+        } else {
+            this.setState({
+                errorPassword: ""
+            })
+        }
+        let usersStorage = localStorage.getItem("users");
+        if (usersStorage !== null) {
+            let usersParseado = JSON.parse (usersStorage);
+            let usersFiltrado = usersParseado.filter((elemento => elemento.email === this.state.email));
+            if (usersFiltrado.length > 0){
+                this.setState({errorEmail: "Ya existe un usuario con el email ingresado"});
+                return;
+            } else {
+                usersParseado.push (usuarioACrear);
+                let usersEnJson = JSON.stringify (usersParseado);
+                localStorage.setItem ("users", usersEnJson);
+            }
+        } else {
+            let usersInicial = [usuarioACrear];
+            let usersEnJson = JSON.stringify (usersInicial);
+            localStorage.setItem ("users", usersEnJson);
+        }
+        this.props.history.push("/login");
+    }
+
+    render() {
+        return (
+            <form onSubmit={(e) => this.submit(e)}>
+                <div className="form-group">
+                    <input type="text" placeholder = "Username" className="form-control" value={this.state.username} onChange={(e) => this.setState({ username: e.target.value })}/>
+                    {this.state.errorUsername && (<p className="text-danger">{this.state.errorUsername}</p>)}
+                </div>
+               
+                <div className="form-group">
+                    <input type="text" placeholder = "Email" className="form-control" value={this.state.email} onChange={(e) => this.setState({ email: e.target.value })}/>
+                    {this.state.errorEmail && (<p className="text-danger">{this.state.errorEmail}</p>)}
+                </div>
+
+                <div className="form-group">
+                    <input type="password" placeholder = "Password" className="form-control" value={this.state.password} onChange={(e) => this.setState({ password: e.target.value })} />
+                    {this.state.errorPassword && (<p className="text-danger">{this.state.errorPassword}</p>)}
+                </div>
+
+                <button type="submit" className="btn btn-primary btn-block">
+                    Sign Up
+                </button>
+
+            </form>
+        );
+    }
+}
+    
+export default SignUpForm;
--- src/Screens/SignUp/SignUp.js
+++ src/Screens/SignUp/SignUp.js
@@ -1,84 +1,26 @@
 import React, { Component } from 'react';
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
+import SignUpForm from "../../Components/SignUpForm/SignUpForm";
 import './SignUp.css';
 
 class SignUp extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: "",
-            password: "",
-            errorEmail: "",
-            errorPassword: "",
-        };
-    }
-
-    evitarSubmit(event) {
-        event.preventDefault();
-
-        this.setState({ errorEmail: "", errorPassword: "" });
-
-        let email = this.state.email;
-        let password = this.state.password;
-
-        if (password.length < 6) {
-            this.setState({ errorPassword: "La contraseña debe tener mínimo 6 caracteres" });
-            return;
-        }
-
-        let usuarios = localStorage.getItem("usuarios");
-        if (usuarios === null) {
-            usuarios = []
-        } else {
-            usuarios = JSON.parse(usuarios);
-        }
-
-        let emailsExistentes = usuarios.filter((usuario) => usuario.email === email);
-        if (emailsExistentes.length > 0) {
-            this.setState({ errorEmail: "El email ya está en uso." });
-            return;
-        }
-
-        let nuevoUsuario = { email: email, password: password };
-        usuarios.push(nuevoUsuario);
-        let usuariosJSON = JSON.stringify(usuarios);
-        localStorage.setItem("usuarios", usuariosJSON);
-
-        alert("¡Cuenta creada con éxito!");
-    }
-
-    controlarCambios(event) {
-        if (event.target.id === "email") {
-            this.setState({ email: event.target.value });
-        } else if (event.target.id === "password") {
-            this.setState({ password: event.target.value });
-        }
-    }
-
     render() {
         return (
             <div className="row justify-content-center">
                 <video autoPlay muted loop className="video-bg">
                     <source src="/videos/backgroundvideo.mp4" type="video/mp4" />
                 </video>
+
                 <Header />
-                <div className="col-md-6">
-                    <form onSubmit={(event) => this.evitarSubmit(event)}>
-                        <div className="form-group">
-                            <label>Email</label>
-                            <input type="email" className="form-control" id="email" placeholder="Enter your email" onChange={(event) => this.controlarCambios(event)} value={this.state.email} />
-                            {this.state.errorEmail !== "" ? (<p className="text-danger">{this.state.errorEmail}</p>) : ("")}
-                        </div>
-                        <div className="form-group">
-                            <label>Password</label>
-                            <input type="password" className="form-control" id="password" placeholder="Enter your password" onChange={(event) => this.controlarCambios(event)} value={this.state.password} />
-                            {this.state.errorPassword !== "" ? (<p className="text-danger">{this.state.errorPassword}</p>) : ("")}
-                        </div>
-                        <button type="submit" className="btn btn-primary btn-block">Register</button>
-                    </form>
-                    <p className="mt-3 text-center">¿Already have an account? <a href="login.html">Log In</a></p>
+
+                <div className="form-container">
+                    <div className="register-box">
+                        <h2>Sign Up</h2>
+                        <SignUpForm history={this.props.history} />
+                    </div>
                 </div>
+
                 <Footer />
             </div>
         );
