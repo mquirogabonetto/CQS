41a1c36058f7c7382a54fddf68d2f274166bc5c7
mquirogabonetto <mquirogabonetto@udesa.edu.ar>
2026-05-13 12:16:44 -0300

    HomeSection con useState y useEffect

--- src/Components/HomeSection/HomeSection.js
+++ src/Components/HomeSection/HomeSection.js
@@ -1,63 +1,55 @@
-import React, { Component } from "react";
+import { useState, useEffect } from "react";
 import MovieCard from "../MovieCard/MovieCard";
 import Loader from "../../Screens/Loader/Loader";
 import Cookies from "universal-cookie";
 
 const cookies = new Cookies();
 
-class HomeSection extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            datos: [],
-            loading: true
-        };
-    }
+function HomeSection(props) {
+    const [datos, setDatos] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-    componentDidMount() {
+    useEffect(() => {
         const apiKey = "b604e547cd3fb7ac5cc35be72e2e0516";
-        let type = this.props.type;
+        let type = props.type;
         const url = `https://api.themoviedb.org/3/${type}/popular?api_key=${apiKey}`;
+
         fetch(url)
             .then(response => response.json())
             .then(data => {
-                this.setState({
-                    datos: data.results || [],
-                    loading: false
-                });
+                setDatos(data.results || []);
+                setLoading(false);
             })
             .catch(error => {
                 console.log("Error:", error);
-                this.setState({ loading: false });
+                setLoading(false);
             });
+    }, [props.type]);
 
-    }
+    let logueado = cookies.get("auth-user");
 
-    render() {
-        let logueado = cookies.get("auth-user");
-        return (
-            <div>
-                <section className="cardContainer">
-                    {this.state.loading ? (
-                        <Loader />
-                    ) : (
-                        this.state.datos.slice(0, 8).map((movie) => (
-                            <MovieCard
-                                key={movie.id}
-                                id={movie.id}
-                                tipo={this.props.type}
-                                title={movie.title || movie.name}
-                                poster={movie.poster_path}
-                                rating={movie.vote_average}
-                                overview={movie.overview}
-                                logueado={logueado}
-                            />
-                        ))
-                    )}
-                </section>
-            </div>
-        );
-    }
+    return (
+        <div>
+            <section className="cardContainer">
+                {loading ? (
+                    <Loader />
+                ) : (
+                    datos.slice(0, 8).map((movie) => (
+                        <MovieCard
+                            key={movie.id}
+                            id={movie.id}
+                            tipo={props.type}
+                            title={movie.title || movie.name}
+                            poster={movie.poster_path}
+                            rating={movie.vote_average}
+                            overview={movie.overview}
+                            logueado={logueado}
+                        />
+                    ))
+                )}
+            </section>
+        </div>
+    );
 }
 
 export default HomeSection;
