3e75b2e23087a03ac95d6568fba5ae2640e22393
mcittanti <mcittanti@udesa.edu.ar>
2026-05-14 10:21:45 -0300

    Transformacion componente funcional con estado MovieCard

--- src/Components/MovieCard/MovieCard.js
+++ src/Components/MovieCard/MovieCard.js
@@ -1,82 +1,73 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { withRouter, Link } from "react-router-dom";
 import './MovieCard.css'
 
-class MovieCard extends Component {
-  constructor() {
-    super();
-    this.state = {
-      mostrarInfo: false,
-      esFavorito: false
-    }
-  }
+function MovieCard(props) {
+  const [mostrarInfo, setMostrarInfo] = useState(false);
+  const [esFavorito, setEsFavorito] = useState(false);
 
-  componentDidMount() {
-    let tipo = this.props.tipo;
-    let storageKey = tipo === "movie" ? "favoritosMovies" : "favoritosSeries";
+  useEffect(() => {
+    let storageKey = props.tipo === "movie" ? "favoritosMovies" : "favoritosSeries";
     let favoritos = JSON.parse(localStorage.getItem(storageKey)) || [];
-    let existe = favoritos.find(f => f.id === this.props.id);
+    let existe = favoritos.find(f => f.id === props.id);
 
     if (existe) {
-      this.setState({ esFavorito: true });
+      setEsFavorito(true);
     }
+  }, []);
+
+  const mostrar = () => {
+    mostrarInfo: (!setMostrarInfo)
   }
 
-  mostrar() {
-    this.setState({
-      mostrarInfo: !this.state.mostrarInfo
-    })
-  }
-
-  manejarFavorito() {
-    let tipo = this.props.tipo;
+  const manejarFavorito = () => {
+    let tipo = props.tipo;
     let storageKey = tipo === "movie" ? "favoritosMovies" : "favoritosSeries";
     let favoritos = JSON.parse(localStorage.getItem(storageKey)) || [];
-    let existe = favoritos.find(f => f.id === this.props.id);
+    let existe = favoritos.find(f => f.id === props.id);
 
     if (existe) {
-      favoritos = favoritos.filter(f => f.id !== this.props.id);
-      this.setState({ esFavorito: false });
+      favoritos = favoritos.filter(f => f.id !== props.id);
+      setEsFavorito(false);
 
-      if (this.props.onRemove) {
-        this.props.onRemove(this.props.id, tipo);
+      if (props.onRemove) {
+        props.onRemove(props.id, tipo);
       }
 
     } else {
       favoritos.push({
-        id: this.props.id,
-        title: this.props.title,
-        poster: this.props.poster,
-        overview: this.props.overview
+        id: props.id,
+        title: props.title,
+        poster: props.poster,
+        overview: props.overview
       });
-      this.setState({ esFavorito: true });
+      setEsFavorito(true);
     }
     localStorage.setItem(storageKey, JSON.stringify(favoritos));
   }
 
-  render() {
-    return (
-      <div className="character-card">
-        <img src={`https://image.tmdb.org/t/p/w500${this.props.poster}`} alt={this.props.title} />
-        <h4>{this.props.title}</h4>
-        <div className="buttons">
-          <button onClick={() => this.mostrar()}>
-            {this.state.mostrarInfo ? "See less" : "See description"}
-          </button>
-          <Link to={`/Detalle/${this.props.tipo}/${this.props.id}`}>
-            <button>See details</button>
-          </Link>
-        </div>
-        {this.state.mostrarInfo && <p>{this.props.overview}</p>}
-
-        {this.props.logueado && (
-          <button className="btn-favorites" onClick={() => this.manejarFavorito()}>
-            {this.state.esFavorito ? "Remove ❌" : "Add to favorites ⭐"}
-          </button>
-        )}
+  return (
+    <div className="character-card">
+      <img src={`https://image.tmdb.org/t/p/w500${props.poster}`} alt={props.title} />
+      <h4>{props.title}</h4>
+      <div className="buttons">
+        <button onClick={mostrar}>
+          {mostrarInfo ? "See less" : "See description"}
+        </button>
+        <Link to={`/Detalle/${props.tipo}/${props.id}`}>
+          <button>See details</button>
+        </Link>
       </div>
-    )
-  }
+      {this.state.mostrarInfo && <p>{props.overview}</p>}
+
+      {this.props.logueado && (
+        <button className="btn-favorites" onClick={manejarFavorito}>
+          {esFavorito ? "Remove ❌" : "Add to favorites ⭐"}
+        </button>
+      )}
+    </div>
+  )
+
 }
 
 export default withRouter(MovieCard); 
